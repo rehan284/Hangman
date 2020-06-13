80285da72a2609cb5d102cbc53960df1b62e2122
rehan284 <supermanrehan284@gmail.com>
2020-06-13 20:09:53 +0400

    Updates

---
+++ precache-manifest.8a4f41621649ea53624a6b41be6c4b9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6dbcbf99b82a8f3db5818aa9cd4067",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "b4ed511aef74cabb9682",
+    "url": "/Hangman/static/css/main.9cb1a419.chunk.css"
+  },
+  {
+    "revision": "4ded4df339541bfee3c1",
+    "url": "/Hangman/static/js/2.ed512077.chunk.js"
+  },
+  {
+    "revision": "b4ed511aef74cabb9682",
+    "url": "/Hangman/static/js/main.237744f0.chunk.js"
+  },
+  {
+    "revision": "21fd6368dfb93bfffc1e",
+    "url": "/Hangman/static/js/runtime-main.3c11137d.js"
+  }
+]);
